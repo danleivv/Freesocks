c27c7a41985a08ddcef079fdc2caceee94bacfcf
jne915 <jne0915@gmail.com>
2015-10-16 11:23:58 +0800

    bug fix

--- freesocks/myss.py
+++ freesocks/myss.py
@@ -72,7 +72,6 @@ def main():
                 print 'done'
                 sys.exit()
             now = time.localtime()[3:6]
-            print now
             if now[0] == nxt[0] and now[1] == nxt[1] and now[2] == nxt[2]:
                 p.terminate()
                 break
